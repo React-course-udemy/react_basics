6daedad5c50f6d236bc0acdf098f77a75a352908
thepassant <bassant.ayman69@gmail.com>
2020-01-20 16:07:57 +0200

    second update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Passant', age: 24 },
+      { name: 'Adam', age: 29 },
+      { name: 'Salma', age: 22 },
+      { name: 'Osama', age: 16 }
+    ],
+    otherState: 'some othervalue'
+  };
 
-class App extends component {
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Passantooottttaaaa', age: 24 },
+        { name: 'Adam cheese', age: 29 },
+        { name: 'Salloma', age: 22 },
+        { name: 'Ossa', age: 16 }
+      ]
+    });
+  };
   render() {
-    return <div className="App">hiiii I'm bassant</div>;
+    return (
+      <div className="App">
+        <h1>hiiii I'm bassant</h1>
+        <button onClick={this.switchNameHandler}>switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          {' '}
+          my hoppies: is racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        />
+      </div>
+    );
   }
 }
 
